e2489ebf49dcf1b59e6be1cea5c4cd701c6bf161
HM Alva <amasr59@gmail.com>
2020-11-06 00:15:12 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-(function ($) {
-	"use strict";
 
     jQuery(document).ready(function($){
 
 
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
+        //$(".embed-responsive iframe").addClass("embed-responsive-item");
+        //$(".carousel-inner .item:first-child").addClass("active");
         
-        $('[data-toggle="tooltip"]').tooltip();
+        //$('[data-toggle="tooltip"]').tooltip();
 
+$(".header-area").sticky({topspacing:0});
+        $('.nav li a[href^="#"]').on('click', function (e) {
+					e.preventDefault();
 
+					var target = this.hash;
+					var $target = $(target);
+                    var headerH = "30";
 
+					$('html, body').animate({
+						'scrollTop': $target.offset().top -headerH + "px"
+					}, 1000, 'swing');
+				});
+
+				$('body').scrollspy({ target: '.mainmenu',offset:200 });
+        $(".nav li a").click(function(){
+            $(".navbar-collapse").removeClass("in");
+        });
         
-
 
     });
 
 
-    jQuery(window).load(function(){
-
-        
-    });
-
-
-}(jQuery));	
+    
